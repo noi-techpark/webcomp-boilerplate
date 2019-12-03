936ee4f1c634a9d09d7d911abacb61c3b67d7746
Alex Lanz <alex@krumer.it>
2019-12-03 09:40:27 +0100

    Initial commit

---
+++ Jenkinsfile-Release.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'docker/Dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    parameters {
+        string(name: 'VERSION', defaultValue: '1.0.0', description: 'Version')
+    }
+
+    environment {
+        GIT_REPOSITORY = "git@github.com:noi-techpark/webcomp-boilerplate.git"
+    }
+
+    stages {
+        stage('Clean') {
+            steps {
+                sh 'rm -rf dist'
+            }
+        }
+        stage('Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'npm run lint'
+                sh 'npm run test'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'npm run build'
+            }
+        }
+        stage('Git Tag') {
+            steps {
+                ansiColor('xterm') {
+                    sshagent (credentials: ['jenkins_github_ssh_key']) {
+                        sh 'git remote set-url origin ${GIT_REPOSITORY}'
+                        sh 'git add -A'
+                        sh 'git commit -m "Verion ${VERSION}"'
+                        sh 'git tag -s -a ${VERSION} -m "Version ${VERSION}"'
+                        sh 'git push origin HEAD:master'
+                        sh 'git push origin --tags'
+                    }
+                }
+            }
+        }
+    }
+}
