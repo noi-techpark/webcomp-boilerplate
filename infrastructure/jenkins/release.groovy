61d82fe1cf28250136fa97be7c8635e006e9adbf
Peter Moser <p.moser@noi.bz.it>
2020-12-14 21:25:18 +0100

    Add .editorconfig

--- infrastructure/jenkins/release.groovy
+++ infrastructure/jenkins/release.groovy
@@ -12,10 +12,13 @@ pipeline {
         string(name: 'VERSION', defaultValue: '1.0.0', description: 'Version (without a leading "v")', trim: true)
     }
     environment {
-        GIT_REPOSITORY = "git@github.com:noi-techpark/webcomp-meteo-generic.git"
-        GIT_BRANCH = "infra"
-        DIST_FOLDER = "dist"
-        YOUR_ENVVAR = "..."
+        // Web Components' default variables
+        WC_GIT_REMOTE = GIT_BRANCH.split("/")[0]
+        WC_GIT_BRANCH = GIT_BRANCH.split("/")[1]
+        WC_DIST_PATH = "dist"
+
+        // Your environmental variables
+        // ....
     }
     // TODO: Delete either all yarn or all npm scripts
     stages {
@@ -67,15 +70,15 @@ pipeline {
                         ssh-keyscan -H github.com >> ~/.ssh/known_hosts
                         git config --global user.email "info@opendatahub.bz.it"
                         git config --global user.name "Jenkins"
-                        git remote set-url origin ${GIT_REPOSITORY}
-                        git add ${DIST_FOLDER}/*
+                        git remote set-url ${WC_GIT_REMOTE} ${GIT_URL}
+                        git add ${WC_DIST_PATH}/*
                         git add -A
                         git commit --allow-empty -m "Version ${VERSION}"
                         git tag --delete v${VERSION} || true
-                        git push origin :v${VERSION} || true
+                        git push ${WC_GIT_REMOTE} :v${VERSION} || true
                         git tag -a v${VERSION} -m "Version ${VERSION}"
-                        git push origin HEAD:${GIT_BRANCH}
-                        git push origin v${VERSION}
+                        git push ${WC_GIT_REMOTE} HEAD:${WC_GIT_BRANCH}
+                        git push ${WC_GIT_REMOTE} v${VERSION}
                     """
                 }
             }
