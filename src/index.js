bca827505fcab3a51ea81b93e11ba092c45f400b
delia mennitti <delia.mennitti@stud-inf.unibz.it>
2022-07-13 14:23:27 +0200

    created helloworld

---
+++ src/index.js
@@ -0,0 +1,64 @@
+
+class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+
+        // We need an encapsulation of our component to not 
+        // interfer with the host, nor be vulnerable to outside 
+        // changes --> Solution = SHADOW DOM
+        this.shadow = this.attachShadow(
+            {mode: "open"}    // Set mode to "open", to have access to 
+                              // the shadow dom from inside this component
+        );
+    }
+
+    // Attributes we care about getting values from
+    // Static, because all HelloWorld instances have the same
+    //   observed attribute names
+    static get observedAttributes() {
+        return ['title'];
+    }
+
+    // Override from HTMLElement 
+    // Do not use setters here, because you might end up with an endless loop
+    attributeChangedCallback(propName, oldValue, newValue) {
+        console.log(`Changing "${propName}" from "${oldValue}" to "${newValue}"`);
+        if (propName === "title") {
+            this.render();
+        }
+    }
+
+    // We should better use such getters and setters and not
+    // internal variables for that to avoid the risk of an
+    // endless loop and to have attributes in the html tag and
+    // Javascript properties always in-sync.
+    get title() {
+        return this.getAttribute("title");
+    }
+
+    set title(newTitle) {
+        this.setAttribute("title", newTitle)
+    }
+
+    // Triggers when the element is added to the document *and*
+    // becomes part of the page itself (not just a child of a detached DOM)
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadow.innerHTML = `
+            <style> 
+                h1 {
+                    color: black; 
+                } 
+            </style>
+            <h1>
+                ${this.title}
+            </h1>
+        `;
+    }
+}
+
+// Register our first Custom Element named <hello-world>
+customElements.define('hello-world', HelloWorld);
